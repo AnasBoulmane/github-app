866ec0a27afc11c6bbe90ee256a00abacc76531c
aboulmane <aboulmane@sqli.com>
2019-10-31 20:20:02 +0100

    Updates

---
+++ precache-manifest.baed067dcbe75e2c074cd34275f23e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e36ed86133081c4a6bf77d573e54e9",
+    "url": "/github-app/index.html"
+  },
+  {
+    "revision": "265650435cd91c34156b",
+    "url": "/github-app/static/css/main.1e8dab3a.chunk.css"
+  },
+  {
+    "revision": "aba00fca7fb48fa1ad52",
+    "url": "/github-app/static/js/2.80c49b29.chunk.js"
+  },
+  {
+    "revision": "265650435cd91c34156b",
+    "url": "/github-app/static/js/main.05ff429c.chunk.js"
+  },
+  {
+    "revision": "0da98f7d1a61d3223cdd",
+    "url": "/github-app/static/js/runtime-main.3a0fa404.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-app/static/media/logo.25bf045c.svg"
+  }
+]);
