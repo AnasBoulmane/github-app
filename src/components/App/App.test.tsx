544fff03323591dcfe1ad80775f19fa015fca563
aboulmane <aboulmane@sqli.com>
2019-10-31 20:42:48 +0100

    Enzyme configuration

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,20 @@
+import { shallow } from "enzyme";
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App /> rendering test", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders edit message", () => {
+    const wrapper = shallow(<App />);
+    const msg = (
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    );
+    // expect(wrapper.contains(welcome)).toBe(true);
+    expect(wrapper.contains(msg)).toEqual(true);
+  });
 });
